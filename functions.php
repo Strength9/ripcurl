213bd2a912aa5ee8def95dfcde4975be17eccec1
Strength9 <Strength9@users.noreply.github.com>
2025-05-19 17:34:13 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
 // Theme text domain
-$text_domain = 'ripcurl_theme';
+$text_domain = 'ripcurl';
 
-function ripcurl_setup() {
+function rip_setup() {
     add_theme_support('block-editor-styles');
     add_theme_support('wp-block-styles');
     add_theme_support('editor-styles');
@@ -11,13 +11,13 @@ function ripcurl_setup() {
     add_theme_support('responsive-embeds');
     add_theme_support('custom-logo');
 }
-add_action('after_setup_theme', 'ripcurl_setup');
+add_action('after_setup_theme', 'ripc_setup');
 
-function ripcurl_enqueue_editor_styles() {
+function rip_enqueue_editor_styles() {
     add_theme_support('editor-styles');
     add_editor_style('editor-style.css');
 }
-add_action('after_setup_theme', 'ripcurl_enqueue_editor_styles');
+add_action('after_setup_theme', 'rip_enqueue_editor_styles');
 
 
 
